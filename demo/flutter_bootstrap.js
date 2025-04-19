2fea9070fa2e232b8468cc73ebb06607101d4a0b
wanjawischmeier <wanja.wischmeier@gmail.com>
2025-04-19 10:31:32 +0200

    build 2

--- demo/flutter_bootstrap.js
+++ demo/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3046658477"
+    serviceWorkerVersion: "3898231783"
   }
 });
